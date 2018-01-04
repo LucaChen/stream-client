44b1fd80d3cb610c042009d704e1b8814d7e1655
exp0nge <mdislamwork@gmail.com>
2018-01-04 18:34:41 -0500

    Serialize capture for upstream

--- app.py
+++ app.py
@@ -13,25 +13,49 @@
 # 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
 # 2. Run "python main.py".
 # 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response
+import io
+import json
+import base64
+
+from flask import Flask, render_template, Response, jsonify, make_response
+import requests
+import numpy as np
+import cv2
+
 from camera import VideoCamera
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n\r\n')
+
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/detect')
+def detect():
+    cam = VideoCamera()
+    try:
+        image = cam.get_frame()
+    except IOError:
+        return make_response(jsonify({'status': 'error', 'message': 'failed to read camera'}), 500)
+    detections = requests.post('http://localhost:5001/detect',
+                               data={'b64image': base64.b64encode(image)})
+    return jsonify(detections.json())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
@@ -9,14 +10,13 @@ class VideoCamera(object):
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
-    
+
     def __del__(self):
         self.video.release()
-    
+
     def get_frame(self):
         success, image = self.video.read()
-	# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
+        if not success:
+            raise IOError('Failed to read camera')
         ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        return jpeg
