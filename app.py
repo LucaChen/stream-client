a952ea22d5c7be195fa77a1de582c0cbb95a56cc
exp0nge <mdislamwork@gmail.com>
2018-01-07 23:04:17 -0500

    Add frame route

--- app.py
+++ app.py
@@ -55,6 +55,12 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@app.route('/frame')
+def get_frame():
+    return (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + CAMERA.get_frame().tobytes() + b'\r\n\r\n')
+
+
 @app.route('/stream-detect')
 def detect():
     # http://flask.pocoo.org/docs/0.12/patterns/streaming/
